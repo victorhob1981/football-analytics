9d72db00a9839922a048e1308dd4fca4a2008a34
Victor Hugo Oliveira <victorhob23@gmail.com>
2026-07-31 22:40:06 -0300

    test: fix dbt singular test syntax

--- platform/dbt/tests/test_player_serving_summary_counts_and_dates.sql
+++ platform/dbt/tests/test_player_serving_summary_counts_and_dates.sql
@@ -5,4 +5,4 @@ where competition_count < 0
    or season_count < 0
    or team_count < 0
    or matches_played < 0
-   or career_start_at > career_end_at;
+   or career_start_at > career_end_at
--- platform/dbt/tests/test_team_serving_summary_counts_and_dates.sql
+++ platform/dbt/tests/test_team_serving_summary_counts_and_dates.sql
@@ -4,4 +4,4 @@ from {{ ref('team_serving_summary') }}
 where competition_count < 0
    or season_count < 0
    or matches_played < 0
-   or first_match_at > last_match_at;
+   or first_match_at > last_match_at
